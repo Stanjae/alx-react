9f46847ecb049b03263a3905b03a778fa5150cc6
stanjae <stanykhay29@gmail.com>
2024-07-24 12:46:50 +0100

    lol

--- 0x01-react_intro/task_0/dashboard/src/index.js
+++ 0x01-react_intro/task_0/dashboard/src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Notifications from './Notifications';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <div className="root-notifications">
+      <Notifications />
+    </div>
   </React.StrictMode>
 );
 
@@ -14,3 +19,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
