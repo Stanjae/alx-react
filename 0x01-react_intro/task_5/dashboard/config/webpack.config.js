e946122552fcc625519c8ddcd8b098dc66d41e47
stanjae <stanykhay29@gmail.com>
2024-07-25 01:13:50 +0100

    lll

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -45,6 +45,17 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(jsx|js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [require('@babel/plugin-proposal-object-rest-spread')]
+          }
+        }
+      }
     ],
   },
-};
+}
